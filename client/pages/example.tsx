43624d5885b6b114527469742497cb9b7df6824e
Javier Martínez <javi@javimartinez.es>
2023-11-03 17:07:47 +0100

    Add more context

--- client/pages/example.tsx
+++ client/pages/example.tsx
@@ -3,6 +3,7 @@
 export default function Example() {
     return (
         <main>
+            <h1>This is using PAGES router</h1>
             <h3>Another route distinct than root</h3>
         </main>
     )
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
+        <h1>This is using PAGES router</h1>
+
         <Link href="/example">
           Navigate to SPA page
         </Link>
